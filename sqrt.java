b37b03d76ffb2f2fae553d03115f96e5d2cd4528
N mohammad shaaz <mohammadshaaz457@gmail.com>
2024-12-05 14:24:44 +0530

    Add files via upload

---
+++ sqrt.java
@@ -0,0 +1,13 @@
+public class sqrt {
+
+    public static void main (String args[])
+    {
+
+        double n=14.0;
+        double res = Math.sqrt(n);
+        {
+            System.out.println("sqrt of n is:"+res);
+        }
+        }
+    
+}
